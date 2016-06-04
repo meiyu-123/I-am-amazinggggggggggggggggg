a408d7fc3d8418dd1a478f13809de83c168d5c3b
meiyu-123 <meiyuosullivan@gmail.com>
2016-06-04 14:59:37 +0100

    we got the random number gauge thingy working. we added the bluetooth stuff.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,6 @@
 angular.module('app.controllers', [])
   
-.controller('homeCtrl', function($scope) {
-
-})
-   
-.controller('weighYourBagCtrl', function($scope) {
+.controller('homeCtrl', function($scope,$interval) {
 
   g = new JustGage({
     id: "gauge",
@@ -13,10 +9,56 @@ angular.module('app.controllers', [])
     max: 100,
     title: "WEIGHT",
     titleFontColor:"yellow",
-    labelFontColor: "yellow"
+    labelFontColor: "yellow"	
+
   });
 ;
-           
+
+function changegauge() {
+
+
+var weightofbag  = Math.floor(Math.random() * 1000);
+
+g.refresh (weightofbag) ;
+
+}
+
+// $interval( changegauge, 1000);
+
+
+
+function connectSuccess (thebackpack)  { 
+	alert('connect');
+	ble.read (thebackpack.id,"19B10010-E8F2-537E-4F6C-D104768A1214","19B10001-E8F2-537E-4F6C-D104768A1214",updategauge);
+
+
+}
+
+function connectFailure (p) {
+alert(JSON.stringify(p));
+}
+
+
+function updategauge (weightofbag) {
+
+	g.refresh (weightofbag) ;
+	$scope.start();
+}
+
+$scope.start = function() {
+	alert('start');
+	    ble.connect("19B10010-E8F2-537E-4F6C-D104768A1214", connectSuccess, connectFailure );
+	   alert('stop');
+}
+
+		// body...
+	
+//ble.connect("19B10010-E8F2-537E-4F6C-D104768A1214", connectSuccess, connectFailure );
+    alert('ble')       ;
+
+})
+   
+.controller('weighYourBagCtrl', function($scope) {
 
 
 })
